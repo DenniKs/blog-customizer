2a1bc363dd4ef438dd0bf51eb36b5bd1d2d6fd02
Denis Akhmetdinov <denisahm@gmail.com>
2026-04-19 20:28:48 +0500

    feat: готова проектная работка 10 спринта

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -12,20 +12,29 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+const getArticleStyleVariables = (
+	articleState: typeof defaultArticleState
+): CSSProperties =>
+	({
+		'--font-family': articleState.fontFamilyOption.value,
+		'--font-size': articleState.fontSizeOption.value,
+		'--font-color': articleState.fontColor.value,
+		'--container-width': articleState.contentWidth.value,
+		'--bg-color': articleState.backgroundColor.value,
+	} as CSSProperties);
+
 const App = () => {
+	const [appliedArticleState, setAppliedArticleState] =
+		useState(defaultArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			style={getArticleStyleVariables(appliedArticleState)}>
+			<ArticleParamsForm
+				initialState={defaultArticleState}
+				onApply={setAppliedArticleState}
+			/>
 			<Article />
 		</main>
 	);
